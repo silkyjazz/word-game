241e1526de836086cebc6828de7fbbeb3e0e91cf
Jasmine Ulloa <ulloajazz6@gmail.com>
2023-01-03 14:28:39 -0800

    added timer

--- script.js
+++ script.js
@@ -4,13 +4,18 @@ pseudo-code
 I want to see a button on the html so that i can start the game
 i want an input field to enter 
 */
+var timerText = document.getElementById("timerText") 
 
-var wordBank = [
-    {
-        word: "cat",
-        letter1: "c",
-        letter2: "a",
-        letter3: "t",
+function timer(){
+    var secondsLeft = 60
+    var setTimer = setInterval(function (){
+        secondsLeft--
+        timerText.textContent = "Time left: " + secondsLeft
+        timerText.setAttribute("style", "color: red")
+        if (secondsLeft ===0){
+            clearInterval(setTimer)
+        }
+    }, 1000)
+}
 
-    }
-]
+timer()
