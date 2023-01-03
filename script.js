0f8cb13cd765380fa4330bd41cc039753ff4b147
Darren Watanabe <watanabedarren@yahoo.com>
2023-01-03 15:16:46 -0800

    resolved merge conflicts

--- script.js
+++ script.js
@@ -12,19 +12,6 @@ wordText.innerHTML = wordBank[0];
 function startGame() {
  
 
-<<<<<<< HEAD
-    }
-]
-
-
-function counter() {
-    var wins = w;
-    var losses = l;
-    for (let w = 0; w < winCount; w++) {
-        for (let l = 0; l < lossCount; l++) {
-
-}
-=======
   function timer() {
     console.log("hello");
     var secondsLeft = 60;
@@ -53,4 +40,3 @@ function compare(userWord, word, win, loss){
 // I want the timer to start counting down when the start button is pressed
 
 startButton.addEventListener("click", startGame);
->>>>>>> 9741fcaede880aab5094132e864f5b9c83000194
