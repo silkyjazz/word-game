4468c52602191908821c219787b33277bca105cd
Jasmine Ulloa <ulloajazz6@gmail.com>
2023-01-03 14:05:08 -0800

    added cards to html

---
+++ script.js
@@ -0,0 +1,16 @@
+/* 
+pseudo-code
+
+I want to see a button on the html so that i can start the game
+i want an input field to enter 
+*/
+
+var wordBank = [
+    {
+        word: "cat",
+        letter1: "c",
+        letter2: "a",
+        letter3: "t",
+
+    }
+]
